b6ccbd8a9586d534b966f9df4f18cf3689b24371
kinjals0130 <73653330+kinjals0130@users.noreply.github.com>
2021-12-27 18:32:03 -0500

    created pop ups for each p&t

---
+++ canada_vac.js
@@ -0,0 +1,26 @@
+// Get the modal
+window.onload = function(){
+
+
+var modals = document.getElementsByClassName("modal");
+
+// Get the button that opens the modal
+var btns = document.getElementsByClassName("places");
+
+
+// Get the <span> element that closes the modal
+var spans = document.getElementsByClassName("close");
+
+
+for(let i = 0; i < spans.length; i++){
+    spans[i].onclick = function(){
+        modals[i].style.display = "none";
+    }
+}
+};
+
+$('.btn').click(function(){
+    $('.popup').hide();
+    $('#' + $(this).attr('data-popup')).show();
+  });
+
